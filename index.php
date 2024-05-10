7a49124d00f406b747980b8f11d76c678eccad9c
LunaVHM <lunavaleriahdz.moreno.1e.ce48@gmail.com>
2024-05-09 21:19:02 -0600

    upedate

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Luna Valeria Hernández Moreno</p>'; 
+    echo '<p>Colonia: Rio de las almas</p>'; 
+    echo '<p>Calle: 38</p>'; 
+    echo '<p>Número de casa: #8743</p>'; 
+    echo '<p>Municpio: Sinaloa</p>'; 
+    echo '<p>Telefono: 8446698523</p>'; 
+    ?> 
+
+</html>
---
+++ readme.txt
@@ -0,0 +1,8 @@
+inicio del proceso
+imprimir "Luna Valeria Hernández Moreno"
+imprimir "Colonia: Rio de las almas"
+imprimir "Calle: 38"
+imprimir "Número de casa: #8743"
+imprimir "Municpio: Sinaloa"
+imprimir "Telefono: 8446698523" 
+fin del proceso
